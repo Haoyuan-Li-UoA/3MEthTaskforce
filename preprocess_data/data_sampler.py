45e61e71da26028ff94718f70c1e6d6160325cf2
nine-mink <bournelearner@gmail.com>
2024-09-05 20:27:35 +1200

    data sampler update

---
+++ preprocess_data/data_sampler.py
@@ -0,0 +1,14 @@
+import os
+from preprocess_data import DATASETS
+from original_data_treatment import original_data_treatment
+
+# 调用处理函数
+original_data_treatment(token_num=1000,
+                        sparse=True,
+                        random_sample=False,
+                        dense=False,
+                        token_list=None)
+
+# 处理DATASETS中的每个name
+for name in DATASETS:
+    os.system(f'python preprocess_data.py  --dataset_name {name}')
